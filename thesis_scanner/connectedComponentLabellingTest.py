60fa6a5954aad96f16e8b6a97b6bcbf3d1f3e0aa
Luca Lanzo <luca.lanzo21@gmail.com>
2020-04-18 21:57:58 +0200

    Added contouring, a contour filter for contours that don't represent text and a function that checks if a picture is on its side and turns it upright

--- thesis_scanner/connectedComponentLabellingTest.py
+++ thesis_scanner/connectedComponentLabellingTest.py
@@ -1,13 +1,37 @@
+'''
+Required packages:
+    OpenCV: pip install opencv-python
+    Numpy: pip install numpy
+    imUtils: pip install imutils
+'''
+
 import cv2
 import numpy as np
+import imutils as mute
+
+def turn_image(image):
+    '''
+    Checks if the image is on its side and turns it by 90 degrees counterclockwise.
+    This program only works with upright pictures. Upside down pictures will work too, though.
+    '''
+    height, width, _ = image.shape
+
+    if (height < width):
+        for angle in np.arange(0, 360, 90):
+	        image = mute.rotate(image, angle)
+
+        for angle in np.arange(0, 360, 90):
+	        image = mute.rotate_bound(image, angle)
+
+    return image
 
 
-def thresh_image(image):
+def thresh_image(image_turned):
     '''
     Converts image from BGR to grayscale
     Then converts grayscale image to binary using an adaptive threshold
     '''
-    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image_gray = cv2.cvtColor(image_turned, cv2.COLOR_BGR2GRAY)
     image_thresh = cv2.adaptiveThreshold(image_gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 55, 25)
 
     return image_thresh
@@ -35,24 +59,37 @@ def erode_image(image_dilate):
 
 def find_contours(image_erode):
     '''
-    '''
-    contours, _ = cv2.findContours(image_erode, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    return contours
+    This funtion will run an algorithm on the binary picture, which will then return all the contours on the text and gets rid of false contours
 
-    combed_contours = np.array()
+    findContours function:  image = image on which cv2 is supposed to find the contours
+                            retrieval mode = RETR_EXTERNAL: retrieves only extreme outer contours 
+                            approx mode = CHAIN_APPROX_NONE: stores all contour points
+    boundingRect funtion: On each contour, get the corresponding rectangle with the starting x/y, as well as the height/width of the rectangle
+    '''
+
+    contours, _ = cv2.findContours(image_erode, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    combed_contours = []
 
     for contour in contours:
         xbottom, ybottom, width, height = cv2.boundingRect(contour)
 
-        if (width < 15):
+        # If contour is too small or too high, discard it
+        if (height < 4 or width < 4 or height > 50):
             continue
+
+        combed_contours.append(contour)
+
+    return combed_contours
 
 
 def main():
     debug = True
 
     # load image
-    image = cv2.imread("D:\\Coding\\thesis-scanner\\data\\1.png")
+    image = cv2.imread("D:\\Coding\\thesis-scanner\\data\\Deckblatt5.jpg")
+    # check if image is on its side
+    image = turn_image(image)
     # threshold the image
     image_thresh = thresh_image(image)
     # dilate the image
@@ -63,7 +100,7 @@ def main():
     image_contours = find_contours(image_erode)
 
     if debug == True:
-        cv2.drawContours(image, image_contours, -1, (255, 0, 0), 1)
+        cv2.drawContours(image, image_contours, -1, (0, 0, 255), 2)
         
         cv2.imshow('Imageview', image)
         cv2.waitKey()

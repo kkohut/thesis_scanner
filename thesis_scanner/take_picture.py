6cac0b3fe9057d0bd44a7b3e0bb8f04603b787d5
Daniel Rindin <daniel.rindin@student.fhws.de>
2020-05-12 16:17:06 +0200

    split the function into multiples: initialize_camera() & process()

--- thesis_scanner/take_picture.py
+++ thesis_scanner/take_picture.py
@@ -8,40 +8,48 @@ by Daniel Rindin
 
 import cv2 #using opencv version 4.2.0 for python 3.8
 
-print("starting...")
+def initialize_camera(width ,height):
+    #creates object using the first camera listed (0)
+    cam = cv2.VideoCapture(0)
+    #set Resolution
+    cam.set(3,width)
+    cam.set(4,height)
+    return cam
 
-#creates object using the first camera listed (0)
-cam = cv2.VideoCapture(0)
-#set Resolution
-cam.set(3,1080)
-cam.set(4,720)
-#creates new window called "test"
-cv2.namedWindow("test")
-#img counter if more than one pictures are taken
-img_counter = 0
+def process():
+    print("starting...")
 
-while True:
-    #reads the input from the camera and shows it in the window "test"
-    ret, frame = cam.read()
-    cv2.imshow("test", frame)
-    if not ret:
-        break
+    cam = initialize_camera(1080,720)
 
-    #waits for a key to be pressed
-    k = cv2.waitKey(1)
+    #creates new window called "test"
+    cv2.namedWindow("test")
+    #img counter if more than one pictures are taken
+    img_counter = 0
 
-    if k%256 == 27:
-        # ESC pressed
-        print("Escape hit, closing...")
-        break
-    elif k%256 == 32:
-        # SPACE pressed
-        img_name = "opencv_frame_{}.png".format(img_counter)
-        #saves the image
-        cv2.imwrite(img_name, frame)
-        print("{} saved!".format(img_name))
-        img_counter += 1
+    while True:
+        #reads the input from the camera and shows it in the window "test"
+        ret, frame = cam.read()
+        cv2.imshow("test", frame)
+        if not ret:
+            break
 
-cam.release()
+        #waits for a key to be pressed
+        k = cv2.waitKey(1)
 
-cv2.destroyAllWindows()
+        if k%256 == 27:
+            # ESC pressed
+            print("Escape hit, closing...")
+            break
+        elif k%256 == 32:
+            # SPACE pressed
+            img_name = "opencv_frame_{}.png".format(img_counter)
+            #saves the image
+            cv2.imwrite(img_name, frame)
+            print("{} saved!".format(img_name))
+            img_counter += 1
+
+    cam.release()
+
+    cv2.destroyAllWindows()
+
+process()

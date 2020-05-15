fdcae724db3d8e35a6e95a9facdd62b7b398702a
k45427 <alexander.bayerlein@student.fhws.de>
2020-05-15 16:26:05 +0200

    changed GUI

--- thesis_scanner/Bounding_boxes.py
+++ thesis_scanner/Bounding_boxes.py
@@ -1,18 +1,18 @@
 import cv2
 import pytesseract as tess
 
-img = cv2.imread(r"C:\Users\alexb\Documents\thesis-scanner\data\testOhneFolie08.jpg")
-hImg, wImg, _ = img.shape  # returns rows, columns and channels(colour)
-textBoxes = tess.image_to_data(img, "deu")
+#image = cv2.imread(r"C:\Users\alexb\Documents\thesis-scanner\data\testOhneFolie08.jpg")
 
 
-def getImageWithBoundingBoxes(boxes):
+
+def getImageWithBoundingBoxes(image):
     """
 
     :param boxes: List with the following columns
     dict_keys(['level', 'page_num', 'block_num', 'par_num', 'line_num', 'word_num', 'left', 'top', 'width', 'height', 'conf', 'text'])
     :return:
     """
+    textBoxes = tess.image_to_data(image, "deu")
 
     for x, b in enumerate(textBoxes.splitlines()):  # enumarate counts the lines
 
@@ -21,11 +21,11 @@ def getImageWithBoundingBoxes(boxes):
             if (len(b) == 12):
                 print(b)
                 x, y, w, h = int(b[6]), int(b[7]), int(b[8]), int(b[9])
-                cv2.rectangle(img, (x - 3, y - 3), (w + x + 3, h + y + 3), (0, 127, 255), 3)
+                cv2.rectangle(image, (x - 3, y - 3), (w + x + 3, h + y + 3), (0, 127, 255), 3)
+    return image
 
 
-getImageWithBoundingBoxes(textBoxes)
-
-cv2.namedWindow("Result", cv2.WINDOW_NORMAL)
-cv2.imshow("Result", img)
-cv2.waitKey()
+# getImageWithBoundingBoxes(image)
+# cv2.namedWindow("Result", cv2.WINDOW_NORMAL)
+# cv2.imshow("Result", image)
+# cv2.waitKey()

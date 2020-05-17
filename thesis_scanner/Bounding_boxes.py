9ec28db3a95167d45957f503b1f951e6b48ffb1e
k45427 <alexander.bayerlein@student.fhws.de>
2020-05-17 11:45:46 +0200

    updated Bounding Boxes

--- thesis_scanner/Bounding_boxes.py
+++ thesis_scanner/Bounding_boxes.py
@@ -4,7 +4,6 @@ import pytesseract as tess
 #image = cv2.imread(r"C:\Users\alexb\Documents\thesis-scanner\data\testOhneFolie08.jpg")
 
 
-
 def getImageWithBoundingBoxes(image):
     """
 
@@ -15,15 +14,16 @@ def getImageWithBoundingBoxes(image):
     textBoxes = tess.image_to_data(image, "deu")
 
     for x, b in enumerate(textBoxes.splitlines()):  # enumarate counts the lines
-
+        counter = 0
         if (x != 0):  # dont need the first line
             b = b.split()  # lines with words does have 12 columns, without words only 11
             if (len(b) == 12):
-                print(b)
+                #print(b)
                 x, y, w, h = int(b[6]), int(b[7]), int(b[8]), int(b[9])
-                cv2.rectangle(image, (x - 3, y - 3), (w + x + 3, h + y + 3), (0, 127, 255), 3)
+                image = cv2.rectangle(image, (x - 3, y - 3), (w + x + 3, h + y + 3), (0, 127, 255), 3)
+                # name = "boundingbox_{}.png".format(counter)
+                # cv2.imwrite(name, image)
     return image
-
 
 # getImageWithBoundingBoxes(image)
 # cv2.namedWindow("Result", cv2.WINDOW_NORMAL)

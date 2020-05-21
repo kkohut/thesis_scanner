e44202ea3e648badd7f1b2a821521e3fd002dad3
Kohut, Kevin <kevin.kohut@student.fhws.de>
2020-05-21 20:29:10 +0200

    Added relative paths to the run script so it works on every client

--- thesis_scanner/thesis_scanner_run.py
+++ thesis_scanner/thesis_scanner_run.py
@@ -1,32 +1,34 @@
 import cv2
-import GUI
+import os
+#import GUI     # erstmal ohne GUI zum Laufen bringen
 import take_picture
 import timestamp
 import picture_quality_improve
 import alignImage
-import Rotate_jpg_180.py
-import text_extraction
+import Rotate_jpg_180
 import text_analysis
 import deadline_validity
-
 
 # while(true) ergänzen
 def main():
     # GUI starten [Immer an]
-    GUI()
+    #GUI()
 
 
     # Bild aufnehmen
-    image = take_picture.process()
-    cv2.imwrite("thesis_scanner_run_savedImage", image)
+    #image = take_picture.process()     # erstmal ohne Foto zum Laufen bringen
+    #cv2.imwrite("thesis_scanner_run_savedImage", image)
 
 
-    # Timestamp speichern
-    timeStamp = timestamp.get_timestamp()
+    # Timestamp speichern   # erstmal ohne TimeStamp zum Laufen bringen
+    #timeStamp = timestamp.get_timestamp()
 
 
     # Thesis Liste einlesen [abs_file_path = Pfad zur Thesis Liste]
-    thesisData = text_analysis.read_thesis_data(absFilePath)
+    script_dir = os.path.dirname(__file__)
+    rel_path = "../data/thesis_data.txt"
+    abs_file_path = os.path.join(script_dir, rel_path)
+    thesis_data = text_analysis.read_thesis_data(abs_file_path)
 
 
     # Bild Verbesserung

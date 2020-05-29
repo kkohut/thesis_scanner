eacfa84f9dfb9eb3b3110eb06df5bbb26c9367aa
Alexander Bayerlein <alexander.bayerlein@student.fhws.de>
2020-05-29 23:42:03 +0200

    deleted some testlines

--- thesis_scanner/Rotate_jpg_180.py
+++ thesis_scanner/Rotate_jpg_180.py
@@ -27,10 +27,7 @@ def is_rotation_right(image: Image):
     :return:boolean
     """
     text = (tess.image_to_string(image, "deu")).upper()
-    #print(text)
     if "BACHELOR" in text or "MASTER" in text or "THESIS" in text or "ARBEIT" in text:
-        #print("true")
         return True
     else:
-        #print("false")
         return False

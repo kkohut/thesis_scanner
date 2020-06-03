50f8f5d5b7fdcbcfaaa381ec094081badf0b8317
Kohut, Kevin <kevin.kohut@student.fhws.de>
2020-06-03 09:36:30 +0200

    Added setup.py for easier installation.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='thesis_scanner',
+    version='0.9',
+    description='A program that recognizes bachelor and master theses',
+    long_description='This application automatically scans and analyzes cover sheets of bachelor and master thesis in ' \
+                'order to check if it was handed in in time.',
+    packages=['thesis_scanner'],
+    install_requires=['opencv-python', 'pytesseract', 'pillow', 'textdistance', 'imutils', 'numpy', 'datetime',
+                      'pandas', 'kivy']
+)

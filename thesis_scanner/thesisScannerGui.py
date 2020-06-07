38fba447b957b488a0619a38f2eff68258cde654
Alexander Bayerlein <alexander.bayerlein@student.fhws.de>
2020-06-07 09:36:42 +0200

    changed name of methods

--- thesis_scanner/thesisScannerGui.py
+++ thesis_scanner/thesisScannerGui.py
@@ -1,4 +1,3 @@
-import kivy
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.image import Image
@@ -7,43 +6,51 @@ from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.floatlayout import FloatLayout
 
-
 from kivy.uix.camera import Camera
-from kivy.uix.boxlayout import BoxLayout
 
-import take_picture
-import thesis_scanner_run
+#import thesis_scanner.take_picture
+#import thesis_scanner.thesis_scanner_run
 
 # RGB, Opacity
+from thesis_scanner import thesis_scanner_run
+
+# sets the background of every page r,g,b,alpha/opacity
 Window.clearcolor = (1, .58, 0, 1)
+
 Window.maximize()  # for develop process
+# Window.fullscreen = ("auto") # for final use
 
 
-# Window.fullscreen = ("auto") #for final use
-
+# Create all Pages / Screens of the GUI
+# Every class represents one Page
 class Page1(FloatLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        infoLabel = Label(text="1. Insert your thesis in the scanner\n\n"
-                               "Please make sure the cover page is facing up\n\n"
-                               "2. press \"START\"", font_size=30, halign="center",
-                          pos_hint={"center_x": 0.5, "center_y": 0.8})
-        self.add_widget(infoLabel)
+        # Create label for informations
+        info = Label(text="[b]1. Insert your thesis in the scanner\n\n"
+                          "Please make sure the cover page is facing up\n\n"
+                          "2. press \"START\"[/b]", markup=True, font_size=35, halign="center",
+                     pos_hint={"center_x": 0.5, "center_y": 0.8})
+        # add label to the screen, otherwise it will not be displayed
+        self.add_widget(info)
 
+        # create and add info-image to screen
         source = "../data/paper_input.png"
         self.img = Image(source=source, pos_hint={'center_x': .5, 'center_y': .5}, size_hint=(.4, .4))
         self.add_widget(self.img)
 
-        self.startButton = Button(text="START", font_size=30, size_hint=(.25, .15), pos_hint={'center_x': .5, 'y': .1})
-        self.startButton.bind(on_press=self.pushButton_Start)
-        self.add_widget(self.startButton)
+        # create button
+        self.start = Button(text="[b]START[/b]", markup=True, pos_hint={'center_x': .5, 'y': .1}, size_hint=(.25, .15),
+                            font_size=35)
+        # bind function to button
+        self.start.bind(on_release=self.push_button_start)
+        # add button to screen
+        self.add_widget(self.start)
 
-    def pushButton_Start(self, instance):
-        #take_picture.process()
-        #thesis_scanner_app.page2.showScan()
-        #thesis_scanner_app.CameraClass.run()
-        thesis_scanner_app.screenManager.current = "camera"
+    def push_button_start(self, _):
+        thesis_scanner_app.screenmanager.current = "camera"
+
 
 # class CameraClass(App):
 #     def build(self):
@@ -93,110 +100,112 @@ class CameraClass(FloatLayout):
         self.camaraClick.pos_hint = {'x': .25, 'y': .75}
 
         # bind the button's on_press to onCameraClick
-        self.camaraClick.bind(on_press=self.onCameraClick)
+        self.camaraClick.bind(on_release=self.onCameraClick)
 
         # add camera and button to the layout
         self.add_widget(self.cameraObject)
         self.add_widget(self.camaraClick)
 
-
+    def openCamera(self):
+        self.cameraObject = Camera(play=True)
+        self.cameraObject.resolution = (1080, 720)  # Specify the resolution
 
     # Take the current frame of the video as the photo graph
     def onCameraClick(self, *args):
         print("saving")
         self.cameraObject.export_to_png('thesis.png')
-        self.cameraObject.play=False
-        thesis_scanner_app.page2.showScan()
-        thesis_scanner_app.screenManager.current = "second"
+        self.cameraObject.play = True
+        thesis_scanner_app.page2.show_scan()
+        thesis_scanner_app.screenmanager.current = "second"
+
 
 class Page2(FloatLayout):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.TryAgainButton = Button(text="Try Again", size_hint=(.15, .15), pos_hint={'center_x': .25, 'y': .1})
-        self.TryAgainButton.bind(on_press=self.pushButton_TryAgain)
-        self.add_widget(self.TryAgainButton)
+        self.again = Button(text="Try Again", size_hint=(.15, .15), pos_hint={'center_x': .25, 'y': .1})
+        self.again.bind(on_release=self.push_button_try_again)
+        self.add_widget(self.again)
 
-        self.nextStepButton = Button(text="Next Step", size_hint=(.15, .15), pos_hint={'center_x': .75, 'y': .1})
-        self.nextStepButton.bind(on_press=self.pushButton_NextStep)
-        self.add_widget(self.nextStepButton)
+        self.next = Button(text="Next Step", size_hint=(.15, .15), pos_hint={'center_x': .75, 'y': .1})
+        self.next.bind(on_release=self.push_button_next_step)
+        self.add_widget(self.next)
 
-    def showScan(self):
+    def show_scan(self):
         source = "thesis.png"
         self.img = Image(source=source, pos_hint={'center_x': .5, 'y': .1})
-        self.img.reload()  # reloads the new image from disk if user trys it again
+        self.img.reload()  # reloads the new image from disk if user try again
         self.add_widget(self.img)
 
-    def pushButton_TryAgain(self, _):
-        thesis_scanner_app.screenManager.current = "start"
+    def push_button_try_again(self, _):
+        thesis_scanner_app.screenmanager.current = "camera"
 
-    def pushButton_NextStep(self, instance):
-        thesis_scanner_app.page3.analyzeThesis(self.img)
-        thesis_scanner_app.screenManager.current = "third"
+
+    def push_button_next_step(_):
+        thesis_scanner_app.page3.analyze_thesis()
+        thesis_scanner_app.page3.show_analyzed_data()
+        thesis_scanner_app.screenmanager.current = "third"
 
 
 class Page3(FloatLayout):
-    analyzedName = None
-    analyzedThesis = None
+    analyzed_name = None
+    analyzed_thesis = None
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.startAgainButton = Button(text="Try Again", font_size=30, size_hint=(.25, .15),
-                                       pos_hint={'center_x': .25, 'y': .1})
-        self.startAgainButton.bind(on_press=self.pushButton_TryAgain)
-        self.add_widget(self.startAgainButton)
+        self.again = Button(text="Try Again", font_size=30, size_hint=(.25, .15),
+                            pos_hint={'center_x': .25, 'y': .1})
+        self.again.bind(on_release=self.push_button_try_again)
+        self.add_widget(self.again)
 
-        self.saveButton = Button(text="Save", font_size=30, size_hint=(.25, .15), pos_hint={'center_x': .75, 'y': .1})
-        self.saveButton.bind(on_press=self.pushSave_Button)
-        self.add_widget(self.saveButton)
+        self.save = Button(text="Save", font_size=30, size_hint=(.25, .15), pos_hint={'center_x': .75, 'y': .1})
+        self.save.bind(on_release=self.push_button_save)
+        self.add_widget(self.save)
 
-    def analyzeThesis(self, img):
+    def analyze_thesis(self):
+        self.analyzed_name, self.analyzed_thesis = thesis_scanner_run.main()
 
-        self.analyzedName, self.analyzedThesis = thesis_scanner_run.main()
+    def show_analyzed_data(self):
+        info = Label(text="Name: {}\n\nThesis: {}".format(self.analyzed_name, self.analyzed_thesis),
+                     font_size=30,
+                     halign="center",
+                     pos_hint={"center_x": 0.5, "center_y": 0.8})
+        self.add_widget(info)
 
-        self.showAnalyzedData()
+    def push_button_try_again(self, _):
+        thesis_scanner_app.screenmanager.current = "camera"
 
-    def showAnalyzedData(self):
-        infoLabel = Label(text="Name: {}\n\nThesis: {}".format(self.analyzedName, self.analyzedThesis),
-                          font_size=30,
-                          halign="center",
-                          pos_hint={"center_x": 0.5, "center_y": 0.8})
-        self.add_widget(infoLabel)
-
-    def pushButton_TryAgain(self, _):
-        thesis_scanner_app.screenManager.current = "start"
-
-    def pushSave_Button(self, _):
-        thesis_scanner_app.screenManager.current = "start"
+    def push_button_save(self, _):
+        thesis_scanner_app.screenmanager.current = "start"
 
 
 class ThesisScannerApp(App):
     def build(self):
-        self.screenManager = ScreenManager()
+        self.screenmanager = ScreenManager()
 
         self.page1 = Page1()
         screen = Screen(name="start")
         screen.add_widget(self.page1)
-        self.screenManager.add_widget(screen)
+        self.screenmanager.add_widget(screen)
 
         self.camera = CameraClass()
         screen = Screen(name="camera")
         screen.add_widget(self.camera)
-        self.screenManager.add_widget(screen)
-        
+        self.screenmanager.add_widget(screen)
+
         self.page2 = Page2()
         screen = Screen(name="second")
         screen.add_widget(self.page2)
-        self.screenManager.add_widget(screen)
+        self.screenmanager.add_widget(screen)
 
         self.page3 = Page3()
         screen = Screen(name="third")
         screen.add_widget(self.page3)
-        self.screenManager.add_widget(screen)
+        self.screenmanager.add_widget(screen)
 
-        return self.screenManager
+        return self.screenmanager
 
 
 if __name__ == "__main__":

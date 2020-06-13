3d2770ee6e302b4280ed27cce1f75fcd2c5ce617
Alexander Bayerlein <alexander.bayerlein@student.fhws.de>
2020-06-13 13:14:49 +0200

    changed some format things

--- thesis_scanner/bounding_boxes.py
+++ thesis_scanner/bounding_boxes.py
@@ -1,8 +1,6 @@
 import cv2
 import pytesseract as tess
 
-#image = cv2.imread(r"C:\Users\alexb\Documents\thesis-scanner\data\testOhneFolie08.jpg")
-
 
 def getImageWithBoundingBoxes(image):
     """
@@ -18,13 +16,15 @@ def getImageWithBoundingBoxes(image):
         if (x != 0):  # dont need the first line
             b = b.split()  # lines with words does have 12 columns, without words only 11
             if (len(b) == 12):
-                #print(b)
+                # print(b)
                 x, y, w, h = int(b[6]), int(b[7]), int(b[8]), int(b[9])
                 image = cv2.rectangle(image, (x - 3, y - 3), (w + x + 3, h + y + 3), (0, 127, 255), 3)
                 # name = "boundingbox_{}.png".format(counter)
                 # cv2.imwrite(name, image)
     return image
 
+
+# image = cv2.imread(r"C:\Users\alexb\Documents\thesis-scanner\thesis_scanner\thesis_scanner_run_uprightImage.jpg")
 # getImageWithBoundingBoxes(image)
 # cv2.namedWindow("Result", cv2.WINDOW_NORMAL)
 # cv2.imshow("Result", image)

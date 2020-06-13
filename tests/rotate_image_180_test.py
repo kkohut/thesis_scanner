4dda233f737098addf30d30312bec9a5c7e95b37
Alexander Bayerlein <alexander.bayerlein@student.fhws.de>
2020-06-13 13:13:48 +0200

    implemented changes from rotate_image_180

--- tests/rotate_image_180_test.py
+++ tests/rotate_image_180_test.py
@@ -2,24 +2,27 @@ from unittest import TestCase
 
 import cv2
 
-from thesis_scanner.Rotate_jpg_180 import is_rotation_right, rotate_input
+from thesis_scanner.rotate_image_180 import check_rotation, rotate_input
 
 
 class Test(TestCase):
 
     def test_check_rotationTrue(self):
         image = cv2.imread(r"C:\Users\alexb\Documents\thesis-scanner\data\testOhneFolie08.jpg")
-        self.assertTrue(is_rotation_right(image))
+        rotation, text = check_rotation(image)
+        self.assertTrue(rotation)
 
     def test_check_rotationFalse(self):
         image = cv2.imread(r"C:\Users\alexb\Documents\thesis-scanner\data\testAufKopf02.jpg")
-        self.assertFalse(is_rotation_right(image))
+        rotation, text = check_rotation(image)
+        self.assertFalse(rotation)
 
     def test_rotation(self):
         image = cv2.imread(r"C:\Users\alexb\Documents\thesis-scanner\data\testAufKopf02.jpg")
-        rotatedImg = rotate_input(image)
+
+        image_rotated, text = rotate_input(image)
         cv2.namedWindow("Input", cv2.WINDOW_NORMAL)
         cv2.namedWindow("Output", cv2.WINDOW_NORMAL)
         cv2.imshow("Input", image)
-        cv2.imshow("Output", rotatedImg)
+        cv2.imshow("Output", image_rotated)
         cv2.waitKey()

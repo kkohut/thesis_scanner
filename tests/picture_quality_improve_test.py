680ccbe98ecaa2103566bd2f04ca6abb75857117
Florentin Bernhardt <julius.bernhardt.1@@student.fhws.de>
2020-07-24 10:28:06 +0200

    picture_quality_improve + test cleaned and threshold chenges

---
+++ tests/picture_quality_improve_test.py
@@ -0,0 +1,21 @@
+"""
+WORK IN PROGRESS
+Input .jpeg/.png file , Output .tif file
+
+This script is used to improve the quality of a picture before using tesseract
+You put in a .png-file and get a .tif-file out with changed quality.
+by Julius Bernhardt
+"""
+
+
+
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- thesis_scanner/picture_quality_improve.py
+++ thesis_scanner/picture_quality_improve.py
@@ -1,11 +1,10 @@
 """
-WORK IN PROGRESS
 Input .jpeg/.png file , Output .tif file
 
 This script is used to improve the quality of a picture before using tesseract
-You put in a .png-file and get a .tif-file out with changed quality.
 by Julius Bernhardt
 """
+import tempfile
 import tempfile
 
 import cv2
@@ -27,8 +26,7 @@ def remove_noise(image):
 #thresholding
 def thresholding(image):
    return cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 55, 25)
-   #cv2.threshold(image,255 , cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 55, 25)
-   #return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
 
 #blur
 def blur(image):
@@ -53,37 +51,17 @@ def opening(image):
     return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
 
 
-#runscript
-def picture_quality_improve_fast(image):
-    image = cv2.resize(image, None, fx=3, fy=3, interpolation=cv2.INTER_AREA)
-    image = get_grayscale(image)
-    #image = blur(image)
-    #image = remove_noise(image)
-    image = thresholding(image)
-    ##image = dilate(image)
-    #image = erode(image)
-    #image = opening(image)
-    contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    #image = cv2.GaussianBlur(image, (5, 5), 0)
-    return image
 
 #runscript
 def picture_quality_improve(image):
     image = cv2.resize(image, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
     image = get_grayscale(image)
-    #image = blur(image)
-    #image = remove_noise(image)
+    image = blur(image)
+    image = remove_noise(image)
     image = thresholding(image)
-    ##image = dilate(image)
-    #image = erode(image)
-    #image = opening(image)
-    contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    #image = cv2.GaussianBlur(image, (5, 5), 0)
+    image = dilate(image)
+    image = erode(image)
+    image = opening(image)
     return image
 
-script_dir = os.path.dirname(__file__)
-rel_path = "../data/Test_Duck.jpeg"
-abs_file_path = os.path.join(script_dir, rel_path)
-image = cv2.imread(abs_file_path)
-image = picture_quality_improve(image)
-cv2.imwrite("../data/quality_improveThreshold1.tif",image)
+

f0c160d76d14aeb05fd7b6b02dd118dcc6aa2217
Kohut, Kevin <kevin.kohut@student.fhws.de>
2020-08-07 01:52:08 +0200

    Removed unused file

--- thesis_scanner/tesseract_attempt.py
+++
@@ -1,8 +0,0 @@
-try:
-    from PIL import Image
-except ImportError:
-    import Image
-import pytesseract
-
-print(pytesseract.get_tesseract_version())
-print(pytesseract.image_to_string(image = Image.open("/home/vintuh/MEGA/Studium/Module/SS20/Programmierprojekt/Material/Testdateien/vorlage_word_thesis-0.jpeg"), lang = "deu"))
